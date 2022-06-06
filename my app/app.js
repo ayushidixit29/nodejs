36a6eeb78ffebefd6a64795e06f25ea62b7b1419
ayushidixit29 <ayushid296btechcse2024@kccitm.edu.in>
2022-06-06 14:26:58 +0530

    routes

--- my app/app.js
+++ my app/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var ayushiRouter = require('./routes/ayushi');
+var vratikaRouter = require('./routes/vratika');
 var app = express();
 
 // view engine setup
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ayushi', ayushiRouter);
+app.use('/vratika', vratikaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
