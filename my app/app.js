61b803883d751747b35fd057b4c91d28f089064f
ayushidixit29 <ayushid296btechcse2024@kccitm.edu.in>
2022-06-07 20:55:36 +0530

    get and post practice

--- my app/app.js
+++ my app/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ayushiRouter = require('./routes/ayushi');
+var loginRouter = require('./routes/login');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ayushi', ayushiRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
